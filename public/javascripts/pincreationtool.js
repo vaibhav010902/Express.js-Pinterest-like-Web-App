8e39964f6ad1c4892952d639723c0621614a2dcb
Vaibhav Agrawal <vaibhavagrawal1902@gmail.com>
2025-08-10 22:28:52 +0530

    commit

--- public/javascripts/pincreationtool.js
+++ public/javascripts/pincreationtool.js
@@ -2,9 +2,12 @@ const allowPeopleToCommentBtn = document.querySelector(".allow-people-to-comment
 const allowPeopleToCommentToggle = document.querySelector(".allow-people-to-comment-btn p");
 const showSimilarProductsBtn = document.querySelector(".show-similar-products-btn");
 const showSimilarProductsToggle = document.querySelector(".show-similar-products-btn p");
+
+const moreOptionsBtn = document.querySelector("#container-4-p");
+const moreOptionsBtnContent = document.querySelector(".container-5")
 // const allowPeopleToCommentCaptionBtn = document.querySelector(".allow-people-to-comment-caption");
 
-
+console.log(moreOptionsBtn)
 function allowPeopleToCommentToggleFunction(){
     allowPeopleToCommentBtn.classList.toggle("active")
     allowPeopleToCommentToggle.classList.toggle("active")
@@ -17,12 +20,22 @@ function showSimilarProductsToggleFunction(){
 allowPeopleToCommentBtn.addEventListener("click", ()=>{
     allowPeopleToCommentToggleFunction()
 })
-allowPeopleToCommentToggle.addEventListener("click", ()=>{
-    allowPeopleToCommentToggleFunction()
-})
+// allowPeopleToCommentToggle.addEventListener("click", ()=>{
+//     allowPeopleToCommentToggleFunction()
+// })
 showSimilarProductsBtn.addEventListener("click", () =>{
     showSimilarProductsToggleFunction()
 })
-showSimilarProductsToggle.addEventListener("click", () => {
-    showSimilarProductsToggleFunction()
-})
+// showSimilarProductsToggle.addEventListener("click", () => {
+//     showSimilarProductsToggleFunction()
+// })
+moreOptionsBtn.addEventListener("click", (e) => {
+    const btn = document.querySelector("#container-4-p .material-symbols-outlined");
+    moreOptionsBtnContent.classList.toggle("visible");
+
+    if (moreOptionsBtnContent.classList.contains("visible")) {
+        btn.textContent = "keyboard_arrow_up";
+    } else {
+        btn.textContent = "keyboard_arrow_down";
+    }
+});
